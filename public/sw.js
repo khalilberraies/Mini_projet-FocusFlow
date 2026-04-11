d878ca5c9696dc7ea559821d81c647cbfd77ec59
MohamedKhalil Berrais <berraieskhalil@gmail.com>
2026-04-11 22:16:07 +0100

    Add files via upload
    New files added after the changes to the code

---
+++ public/sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'focus-iot-v2';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Don't cache API calls or external models
+  if (event.request.url.includes('cdn.jsdelivr.net') || event.request.url.includes('storage.googleapis.com')) {
+    return;
+  }
+  
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
